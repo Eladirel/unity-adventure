c04cfd44c0674cebfc8dd3334a554b3faaf5ebdc
Eladriel <50425332+Eladirel@users.noreply.github.com>
2020-10-22 19:57:44 -0600

    Primer commit, proyecto completo

---
+++ Adventure/Assets/Scripts/MovimientoPersonaje.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPersonaje : MonoBehaviour
+{
+
+    float speed = 10;
+    float rotSpeed = 80;
+    float gravity = 8;
+
+    public Animator animator;
+    public CharacterController control;
+    Vector3 vector = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        control = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.W))
+        {
+            
+        }
+    }
+}
---
+++ Adventure/Assets/Scripts/SeguimientoPersonaje.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguimientoPersonaje : MonoBehaviour
+{
+    public Transform personaje;
+    public float distanciaX;     
+    public float distanciaY;     
+    public float distanciaZ;
+
+    void Update( ) 
+    {         
+        // Ajusta la posición         
+        this.transform.position = new Vector3(personaje.position.x + distanciaX, personaje.position.y + distanciaY,personaje.position.z + distanciaZ);         
+        // Ajusta la rotación         
+        this.transform.LookAt(personaje); 
+    }
+}
